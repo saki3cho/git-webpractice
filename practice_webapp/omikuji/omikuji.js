bb2ed4df328a7701a16d4ad02d4d4932572403e2
saki3cho <a9531031@eng.kitakyu-u.ac.jp>
2022-03-07 22:18:29 +0900

    [FINISH] Webアプリケーションを作ってみよう-2
    名前が入力されなかったときに名無しさんと表示するようにした。

--- practice_webapp/omikuji/omikuji.js
+++ practice_webapp/omikuji/omikuji.js
@@ -2,6 +2,7 @@ let username;
 let userresult;
 
 username = prompt("お名前を教えてください。")
+if (username == "")username = "名無しさん";
 document.getElementById("name").innerHTML = username;
 
 let rand = Math.floor(Math.random()*5);
